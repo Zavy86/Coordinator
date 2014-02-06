327a648bd987a2a6a3ead8b9b51da04586b3d604
Manuel Zavatta <manuel.zavatta@gmail.com>
2014-02-06 11:05:36 +0100

    fixed dashboard template

--- dashboard/template.inc.php
+++ dashboard/template.inc.php
@@ -10,56 +10,40 @@ if(!$status){$status=1;}
 ?>
 
 <div class="row-fluid">
- 
+
  <!-- Navigation -->
  <ul class="nav nav-tabs">
 
-  <?php 
+  <?php
    echo "<li";if(api_baseName()=="notifications_list.php" && $status==1){echo " class='active'";}
    echo "><a href='notifications_list.php?s=1'";
    echo ">Notifiche</a></li>";
   ?>
-  
-  <?php 
+
+  <?php
    echo "<li";if(api_baseName()=="notifications_list.php" && $status==2){echo " class='active'";}
    echo "><a href='notifications_list.php?s=2'";
    echo ">Archivio notifiche</a></li>";
   ?>
-  
-  <?php
-   if(api_checkPermission("projects","projects_view")){
-    echo "<li";if(api_baseName()=="projects_activities.php"){echo " class='active'";}
-    echo "><a href='projects_activities.php?s=2'";
-    echo ">Incarichi</a></li>";
-   }
-  ?>
-  
-  <?php
-   if(api_checkPermission("organics","addressbook_view")){
-    echo "<li";if(api_baseName()=="addressbook_list.php"){echo " class='active'";}
-    echo "><a href='addressbook_list.php'";
-    echo ">Rubrica aziendale</a></li>";
-   }
-  ?>
-  
+
   <?php /*<li class="pull-right"><img src="<?php echo $GLOBALS['dir']; ?>/core/images/logos/logo.png" style="height:34px"></li>*/ ?>
-  
+
   <?php $g_search=$_GET['s']; ?>
-  
+
   <?php if(api_baseName()=="addressbook_list.php"){ ?>
-  
+
   <form action="addressbook_list.php" method="get">
    <li class="pull-right search">
     <div class="input-append">
      <input type="text" name="s" class="input-large" placeholder="Search" value="<?php echo $g_search; ?>">
-     <?php if($g_search<>NULL){ echo "<a class='btn' href='addressbook_list.php'><i class='icon-remove-sign'></i></a>"; } ?>     
+     <?php if($g_search<>NULL){ echo "<a class='btn' href='addressbook_list.php'><i class='icon-remove-sign'></i></a>"; } ?>
      <button type="submit" class="btn"><i class="icon-search"></i></button>
     </div>
    </li>
   </form>
-  
+
   <?php } ?>
-  
+
  </ul>
 
 <?php if($checkPermission==NULL){content();}else{if(api_checkPermission("dashboard",$checkPermission,TRUE)){content();}} ?>
