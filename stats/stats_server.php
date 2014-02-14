5777eaa2a73e3ba0975f6ad6af452b1ebb59277a
Manuel Zavatta <manuel.zavatta@gmail.com>
2014-02-14 13:58:05 +0100

    Stats fix

--- stats/stats_server.php
+++ stats/stats_server.php
@@ -10,40 +10,39 @@ function content(){
   unset($_SESSION['stats_cpu']);
   unset($_SESSION['stats_cpu_timestamp']);
  }
+ // include configuration file
+ include("config.inc.php");
+ // loop servers
+ if(count($stats_servers)>0){
+  foreach($stats_servers as $index=>$server){
+   // show charts
+   echo "<div class='row-fluid'>\n";
+   echo "<div class='span7'>\n";
+   echo "<h4>".$server." - CPU</h4></center>\n";
+   echo "<img id='imgCPU_".$index."' src='".$server."/cpu.inc.php'>\n";
+   echo "</div><!-- /span7 -->\n";
+   echo "<div class='span5'>\n";
+   echo "<h4>RAM</h4>\n";
+   echo "<img id='imgRAM_".$index."' src='".$server."/ram.inc.php'>\n";
+   echo "</div><!-- /span5 -->\n";
+   echo "</div><!-- /row-fluid -->\n";
+   echo "<br>";
+  }
+ }
 ?>
-<div id="res"></div>
-<div class="row-fluid">
- <div class="span7">
- <h4><?php echo strtoupper($_SERVER['SERVER_NAME']);?> - CPU</h4>
- <img id="imgCPU" src="cpu.inc.php">
-</div><!-- /span7 -->
-<div class="span5">
- <h4><?php echo strtoupper($_SERVER['SERVER_NAME']);?> - RAM</h4>
- <img id="imgRAM" src="ram.inc.php">
-</div><!-- /span5 -->
-</div><!-- /row-fluid -->
-
-<br>
-
-<div class="row-fluid">
- <div class="span7">
- <h4>SIS-MYSQL - CPU</h4>
- <img id="imgCPU-sql" src="http://sis-mysql/scripts/stats/cpu.inc.php">
-</div><!-- /span7 -->
-<div class="span5">
- <h4>SIS-MYSQL - RAM</h4>
- <img id="imgRAM-sql" src="http://sis-mysql/scripts/stats/ram.inc.php">
-</div><!-- /span5 -->
-</div><!-- /row-fluid -->
-
 <script type="text/javascript">
  $(document).ready(function(){
   // refresh charts every 2 sec
   setInterval(function(){
-   $("#imgCPU" ).attr("src","cpu.inc.php");
-   $("#imgRAM" ).attr("src","ram.inc.php");
-   $("#imgCPU-sql" ).attr("src","http://sis-mysql/scripts/stats/cpu.inc.php");
-   $("#imgRAM-sql" ).attr("src","http://sis-mysql/scripts/stats/ram.inc.php");
+   <?php
+    // loop servers
+    if(count($stats_servers)>0){
+     foreach($stats_servers as $index=>$server){
+      echo "$('#imgCPU_".$index."').attr('src','".$server."/cpu.inc.php');\n";
+      echo "$('#imgRAM_".$index."').attr('src','".$server."/ram.inc.php');\n";
+     }
+    }
+   ?>
   },2000);
  });
 </script>
