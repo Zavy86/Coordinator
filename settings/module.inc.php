eaef360f4ec66f27083f30220ffb6bdc1560d8e0
Manuel Zavatta <manuel.zavatta@gmail.com>
2014-02-19 14:24:55 +0100

    Stats upgrade

---
+++ settings/module.inc.php
@@ -0,0 +1,9 @@
+<?php
+/* -------------------------------------------------------------------------- *\
+|* -[ Settings - Module Information ]---------------------------------------- *|
+\* -------------------------------------------------------------------------- */
+$module_name="settings";
+$module_version="1.0.0";
+$module_title="Settings";
+$module_description="Coordinator Settings";
+?>
