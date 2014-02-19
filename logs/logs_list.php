70432a3b3a2d539ea6466fe15e4a6e5882444032
Manuel Zavatta <manuel.zavatta@gmail.com>
2014-02-19 14:04:09 +0100

    Logs upgraded

--- logs/logs_list.php
+++ logs/logs_list.php
@@ -12,39 +12,33 @@ function content(){
  if(!isset($g_typology)){$g_typology=0;}
  $g_module=$_GET['m'];
  if(!isset($g_module)){$g_module=NULL;}
- $g_page=$_GET['p'];
- if(!$g_page){$g_page=1;}
- $g_limit=$_GET['l'];
- if(!isset($g_limit)){$g_limit=20;}
+ // definitions
+ $news_array=array();
+ $modals_array=array();
  // generate query
  $query_where="timestamp BETWEEN CURDATE()- INTERVAL ".($g_interval-1)." DAY AND NOW()";
  if($g_typology>0&&$g_typology<4){$query_where.=" AND typology='".$g_typology."'";}
  if($g_typology==-1){$query_where.=" AND (typology='2' OR typology='3')";}
  if($g_module<>NULL){$query_where.=" AND module='".$g_module."'";}
  // pagination
- if($g_limit>0){
-  $recordsLimit=$g_limit;
-  $recordsCount=$GLOBALS['db']->countOf("logs_logs",$query_where);
-  $query_start=($g_page-1)*$recordsLimit;
-  $query_limit=" LIMIT ".$query_start.",".$recordsLimit;
- }
- // build table header
- $th_array=array(
-  api_tableHeader("&nbsp;",NULL,"16"),
-  api_tableHeader(api_text("list-th-date"),"nowarp"),
-  api_tableHeader(api_text("list-th-module"),"nowarp"),
-  api_tableHeader(api_text("list-th-log"),NULL,"100%"),
-  api_tableHeader(api_text("list-th-account"),"nowarp"),
- );
- // new logs array
- $logs_new_id=array();
+ $pagination=new str_pagination("logs_logs",$query_where,"&i=".$g_interval."&t=".$g_typology."&m=".$g_module);
+ $query_limit=$pagination->queryLimit();
+ // sorting
+ $query_order=api_queryOrder("timestamp DESC");
+ // build table
+ $table=new str_table(api_text("groups_list-tr-unvalued"),TRUE,"&i=".$g_interval."&t=".$g_typology."&m=".$g_module);
+ $table->addHeader("&nbsp;",NULL,"16");
+ $table->addHeader(api_text("list-th-date"),"nowarp",NULL,"timestamp");
+ $table->addHeader(api_text("list-th-module"),"nowarp",NULL,"module");
+ $table->addHeader(api_text("list-th-log"),NULL,"100%","log");
+ $table->addHeader(api_text("list-th-account"),"nowarp");
  // execute query
- $logs=$GLOBALS['db']->query("SELECT * FROM logs_logs WHERE ".$query_where." ORDER BY timestamp DESC".$query_limit);
+ $logs=$GLOBALS['db']->query("SELECT * FROM logs_logs WHERE ".$query_where.$query_order.$query_limit);
  while($log=$GLOBALS['db']->fetchNextObject($logs)){
   $tr_class=NULL;
   // check unread
   if($log->new){
-   $logs_new_id[]=$log->id;
+   $news_array[]=$log->id;
    // set new status to false
    $GLOBALS['db']->execute("UPDATE logs_logs SET new='0' WHERE id='".$log->id."'");
   }
@@ -58,16 +52,9 @@ function content(){
   if(($strpos=strpos($log->log,"\n"))==0){$strpos=50;}
   $log_subject=str_ireplace("<br>"," ",substr($log->log,0,$strpos));
   if($log->new){$log_subject="<span class='unread'>".$log_subject."</span>";}
-  // build table data
-  $td_array=array();
-  $td_array[]=api_tableField(api_icon($td_icon),"nowarp");
-  $td_array[]=api_tableField(api_timestampFormat($log->timestamp,TRUE),"nowarp");
-  $td_array[]=api_tableField(strtoupper(stripslashes($log->module)));
-  $td_array[]=api_tableField(api_modalLink($log->id,$log_subject));
-  $td_array[]=api_tableField(api_accountName($log->idAccount),"nowarp");
-  // build group table row
-  $tr_array[]=api_tableRow($td_array,$tr_class);
   // build modal window
+  $modal=new str_modal($log->id);
+  $modal->header(strip_tags($log_subject));
   $m_body="<dl>\n";
   $m_body.="<dt>Data</dt><dd>".api_timestampFormat($log->timestamp,TRUE,TRUE)."</dd><br>\n";
   if($log->idAccount>0){$m_body.="<dt>Account:</dt><dd>".api_accountName($log->idAccount)."</dd><br>\n";}
@@ -75,18 +62,28 @@ function content(){
   if(strlen($log->link)>0){$m_body.="<dt>Link:</dt><dd><a href='".$GLOBALS['dir'].$log->link."' target='_blank'>".$log->link."</a></dd><br>\n";}
   $m_body.="<dt>Log</dt><dd>".nl2br($log->log)."</dd>\n";
   $m_body.="</dl>";
-  // show modal window
-  api_modal($log->id,strip_tags($log_subject),$m_body);
+  $modal->body($m_body);
+  $modals_array[]=$modal;
+  // build group table row
+  $table->addRow($tr_class);
+  // build table fields
+  $table->addField(api_icon($td_icon),"nowarp");
+  $table->addField(api_timestampFormat($log->timestamp,TRUE),"nowarp");
+  $table->addField(strtoupper(stripslashes($log->module)));
+  $table->addField($modal->link($log_subject));
+  $table->addField(api_accountName($log->idAccount),"nowarp");
  }
  // show table
- api_table($th_array,$tr_array,api_text("groups_list-tr-unvalued"));
- // show the pagination div
- api_pagination($recordsCount,$recordsLimit,$g_page,"logs_list.php?i=".$g_interval."&t=".$g_typology."&m=".$g_module,"pagination pagination-small pagination-right");
+ $table->render();
+ // show modals
+ foreach($modals_array as $modal){$modal->render();}
+ // show pagination
+ $pagination->render();
  // set the javascript to set read new items
- if(count($logs_new_id)>0){
+ if(count($news_array)>0){
   echo "<script language='javascript'>\n";
-  foreach($logs_new_id as $log_new_id){
-   echo "$(\"#modal-link_".$log_new_id."\").click(function(){\n";
+  foreach($news_array as $new){
+   echo "$(\"#modal-link_".$new."\").click(function(){\n";
    echo " $(this).find(\"span\").removeClass('unread')\n";
    echo "});\n";
   }
