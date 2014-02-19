ede90faf98b51bfcf137dfd2f2651efd14014ae9
Manuel Zavatta <manuel.zavatta@gmail.com>
2014-02-19 10:24:23 +0100

    Accounts upgraded with structure class

--- accounts/template.inc.php
+++ accounts/template.inc.php
@@ -6,33 +6,17 @@ include("../core/api.inc.php");
 // show header
 $html->header("Accounts",NULL);
 // build navigation tab
-$nt_array=array();
-$nt_array[]=api_navigationTab(api_text("nav-profile"),"index.php");
-$nt_array[]=api_navigationTab(api_text("nav-accounts"),"accounts_list.php",NULL,NULL,NULL,(api_checkPermission("accounts","accounts_list"))?TRUE:FALSE);
-$nt_array[]=api_navigationTab(api_text("nav-groups"),"groups_list.php",NULL,NULL,NULL,(api_checkPermission("accounts","groups_list"))?TRUE:FALSE);
-$nt_array[]=api_navigationTab(api_text("nav-companies"),"companies_list.php",NULL,NULL,NULL,(api_checkPermission("accounts","accounts_list"))?TRUE:FALSE);
-
-
-if(
- api_baseName()=="accounts_edit.php" ||
- api_baseName()=="groups_edit.php" ||
- api_baseName()=="companies_edit.php"
-){$class="active";}else{$class=NULL;}
-
-if($_GET['id']>0){$label=api_text("nav-edit");}else{$label=api_text("nav-add");}
-
-
-
-$ntd_array=array();
-$ntd_array[]=api_navigationTab(api_text("nav-account"),"accounts_edit.php",NULL,NULL,NULL,(api_checkPermission("accounts","accounts_add"))?TRUE:FALSE);
-$ntd_array[]=api_navigationTab(api_text("nav-group"),"groups_edit.php",NULL,NULL,NULL,(api_checkPermission("accounts","groups_add"))?TRUE:FALSE);
-$ntd_array[]=api_navigationTab(api_text("nav-company"),"companies_edit.php",NULL,NULL,NULL,(api_checkPermission("accounts","companies_add"))?TRUE:FALSE);
-
-$nt_array[]=api_navigationTab($label,NULL,NULL,$class,$ntd_array);
-
+$nav=new str_navigation();
+$nav->addTab(api_text("nav-profile"),"index.php");
+$nav->addTab(api_text("nav-accounts"),"accounts_list.php",NULL,NULL,(api_checkPermission("accounts","accounts_list"))?TRUE:FALSE);
+$nav->addTab(api_text("nav-groups"),"groups_list.php",NULL,NULL,(api_checkPermission("accounts","groups_list"))?TRUE:FALSE);
+$nav->addTab(api_text("nav-companies"),"companies_list.php",NULL,NULL,(api_checkPermission("accounts","accounts_list"))?TRUE:FALSE);
+$nav->addTab(($_GET['id']>0)?api_text("nav-edit"):api_text("nav-add"));
+$nav->addSubTab(api_text("nav-account"),"accounts_edit.php",NULL,NULL,(api_checkPermission("accounts","accounts_add"))?TRUE:FALSE);
+$nav->addSubTab(api_text("nav-group"),"groups_edit.php",NULL,NULL,(api_checkPermission("accounts","groups_add"))?TRUE:FALSE);
+$nav->addSubTab(api_text("nav-company"),"companies_edit.php",NULL,NULL,(api_checkPermission("accounts","companies_add"))?TRUE:FALSE);
 // show navigation tab
-api_navigation($nt_array);
-
+$nav->render();
 // check permissions before displaying module
 if($checkPermission==NULL){content();}else{if(api_checkPermission("accounts",$checkPermission,TRUE)){content();}}
 // show footer
