53951472d295b3a6a94daa8a13a67db777929ead
Manuel Zavatta <manuel.zavatta@gmail.com>
2014-03-06 13:41:48 +0100

    Dashboard fix

--- notifications/widget.inc.php
+++ notifications/widget.inc.php
@@ -8,26 +8,28 @@ api_loadLocaleFile("../notifications/");
 echo "<h4>".api_text("widget-title")."</h4>\n";
 echo "<div class='well well-small well-white'>\n";
 // acquire variables
-$status=$_GET['s'];
-if(!$status){$status=1;}
+$g_status=$_GET['s'];
+if(!$g_status){$g_status=1;}
 // definitions
+$count=0;
 $modals_array=array();
 // build table
-$table=new str_table(api_text("list-tr-no-results"),TRUE,"&s=".$status);
+$table=new str_table(api_text("list-tr-no-results"),TRUE,"&s=".$g_status);
 $table->addHeader("&nbsp;",NULL,"16",NULL);
-$table->addHeader(api_text("list-th-reception-date"),"nowarp",NULL,"created");
-if($status==2){$table->addHeader(api_text("list-th-archived-date"),"nowarp",NULL,"archived");}
-$table->addHeader(api_text("list-th-notification"),NULL,"100%","subject");
+$table->addHeader(api_text("list-th-reception-date"),"nowarp");
+if($g_status==2){$table->addHeader(api_text("list-th-archived-date"),"nowarp");}
+$table->addHeader(api_text("list-th-notification"),NULL,"100%");
 // build query
-$query_where="status='".$status."' AND idAccountTo='".$_SESSION['account']->id."'";
+$query_where="status='".$g_status."' AND idAccountTo='".$_SESSION['account']->id."'";
 $query_limit=" LIMIT 0,10";
 // query order
-if($status==2){$query_order=api_queryOrder("archived DESC");}else{$query_order=api_queryOrder("created ASC");}
+if($g_status==2){$query_order=api_queryOrder("archived DESC");}else{$query_order=api_queryOrder("created ASC");}
 // query
 $notifications=$GLOBALS['db']->query("SELECT * FROM notifications_notifications WHERE ".$query_where.$query_order.$query_limit);
 while($notification=$GLOBALS['db']->fetchNextObject($notifications)){
+ $count++;
  // build modal
- $modal=new str_modal($notification->id);
+ $modal=new str_modal("notification_widget_".$notification->id);
  // modal header
  $modal->header(stripslashes($notification->subject));
  // modal body
@@ -41,7 +43,7 @@ while($notification=$GLOBALS['db']->fetchNextObject($notifications)){
   }
   $m_body.="<p><a href='".$url."' target='_blank'>".$url."</a></p>\n";
  }
- if($status==2){$m_body.="<p>Archiviata da ".api_accountName($notification->idAccountArchived)." il ".api_timestampFormat($notification->archived,TRUE)."</p>";}
+ if($g_status==2){$m_body.="<p>Archiviata da ".api_accountName($notification->idAccountArchived)." il ".api_timestampFormat($notification->archived,TRUE)."</p>";}
  $modal->body($m_body);
  // build modal footer
  $confirm=NULL;
@@ -49,7 +51,6 @@ while($notification=$GLOBALS['db']->fetchNextObject($notifications)){
   $action="notification_archive";
   if($notification->typology==2){
    $confirm="onClick=\"return confirm('".api_text("list-m-confirm")."')\"";
-   //Archiviando questa notifica segnalerai di aver eseguito l\'azione richiesta. Confermi di aver eseguito l\'azione?
    $button=api_text("list-m-archiveAsDone");
   }else{
    $button=api_text("list-m-archive");
@@ -58,7 +59,7 @@ while($notification=$GLOBALS['db']->fetchNextObject($notifications)){
   $action="notification_restore";
   $button="Ripristina";
  }
- $m_footer="<a class='btn' href='submit.php?act=".$action."&id=".$notification->idAction."'".$confirm.">".$button."</a>";
+ $m_footer="<a class='btn' href='../notifications/submit.php?act=".$action."&id=".$notification->idAction."'".$confirm.">".$button."</a>";
  $modal->footer($m_footer);
  $modals_array[]=$modal;
  // build table row
@@ -69,13 +70,14 @@ while($notification=$GLOBALS['db']->fetchNextObject($notifications)){
   case 2:$table->addField(api_icon("icon-ok-sign"),NULL);break;
  }
  $table->addField(substr($notification->created,0,16),"nowarp");
- if($status==2){$table->addField(substr($notification->archived,0,16),"nowarp");}
+ if($g_status==2){$table->addField(substr($notification->archived,0,16),"nowarp");}
  $table->addField($modal->link(stripslashes($notification->subject)),"nowarp");
 }
 // show table
 $table->render();
 // show modal windows
 foreach($modals_array as $modal){$modal->render();}
-echo "<span class='pull-right'><a href='../notifications/notifications_list.php?s=1'>".api_text("list-li-showAll")."</a></span>\n";
+if(!$count){$g_status="2";}
+echo "<span class='pull-right'><a href='../notifications/notifications_list.php?s=".$g_status."'>".api_text("list-li-showAll")."</a></span>\n";
 echo "<br>\n</div>\n";
 ?>
