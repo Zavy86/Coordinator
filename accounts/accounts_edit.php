65f80b97ac74010eafcb954bb1fefec785de7b8e
Manuel Zavatta <manuel.zavatta@gmail.com>
2015-03-16 11:03:52 +0100

    Accounts list and edit upgrade

--- accounts/accounts_edit.php
+++ accounts/accounts_edit.php
@@ -6,135 +6,224 @@ if($_GET['id']>0){$checkPermission="accounts_edit";}else{$checkPermission="accou
 include("template.inc.php");
 function content(){
  // acquire variables
- $g_id=$_GET['id'];
- if(!isset($g_id)){$g_id=0;}
- // get account object
- $account=$GLOBALS['db']->queryUniqueObject("SELECT * FROM accounts_accounts WHERE id='".$g_id."'");
- // split window
+ if(!isset($_GET['idAccount'])){$_GET['idAccount']=0;}
+ // get objects
+ $account=api_accounts_account($_GET['idAccount']);
+ // build account dynamic list
+ if($account->id>1){
+  $account_dl=new str_dl("br","dl-horizontal");
+  $account_dl->addElement(api_text("accounts_edit-ff-avatar"),api_image($account->avatar,"img-polaroid",125,NULL,TRUE));
+ }
+ // build account form
+ $account_form=new str_form("submit.php?act=account_save&idAccount=".$account->id,"post","accounts_edit");
+ // enabled
+ if($account->id>1){
+  $account_form->addField("checkbox","enabled","&nbsp;");
+  $account_form->addFieldOption(1,api_text("accounts_edit-fo-enabled"),($account->enabled?TRUE:FALSE),($account->del?TRUE:FALSE));
+ }
+ // name
+ $account_form->addField("text","name",api_text("accounts_edit-ff-name"),stripslashes($account->name),"input-large",api_text("accounts_edit-ff-name-placeholder"));
+ // account
+ if($account->id<>1){
+  if(api_getOption("ldap")){
+   $account_form->addField("text","account",api_text("accounts_edit-ff-mail"),stripslashes($account->account),"input-xlarge",api_text("accounts_edit-ff-account-placeholder"));
+   $account_form->addField("text","ldap",api_text("accounts_edit-ff-ldap"),stripslashes($account->ldap),"input-large",api_text("accounts_edit-ff-ldap-placeholder"));
+  }else{
+   $account_form->addField("text","account",api_text("accounts_edit-ff-account"),stripslashes($account->account),"input-xlarge",api_text("accounts_edit-ff-account-placeholder"));
+  }
+ }
+ // language
+ if($account->id<>1){
+  $account_form->addField("select","language",api_text("accounts_edit-ff-language"),NULL,"input-medium");
+  foreach(api_language_availables() as $key=>$language){$account_form->addFieldOption($key,$language." (".$key.")",($key==$account->language?TRUE:FALSE));}
+ }
+ // super user
+ if($account->id>1){
+  $account_form->addField("checkbox","superuser","&nbsp;");
+  $account_form->addFieldOption(1,api_text("accounts_edit-fo-superuser"),($account->superuser?TRUE:FALSE),($account->del?TRUE:FALSE));
+ }
+ // controls
+ $account_form->addControl("submit",api_text("accounts_edit-fc-submit"));
+ $account_form->addControl("button",api_text("accounts_edit-fc-cancel"),NULL,"accounts_list.php");
+ if($account->id>0 && api_checkPermission("accounts","accounts_delete")){
+  if($account->del){$account_form->addControl("button",api_text("accounts_edit-fc-undelete"),"btn-warning","submit.php?act=account_undelete&idAccount=".$account->id);}
+  else{$account_form->addControl("button",api_text("accounts_edit-fc-delete"),"btn-danger","submit.php?act=account_delete&idAccount=".$account->id,api_text("accounts_edit-fc-delete-confirm"));}
+ }
+ if($account->id>1 && (($_SESSION['account']->administrator && $account->id<>api_accountId())||api_accountId()==1)){
+  $account_form->addControl("button",api_text("accounts_edit-fc-interpret"),"btn-success","submit.php?act=account_interpret&idAccount=".$account->id,api_text("accounts_edit-fc-interpret-confirm"));
+ }
+ // companies
+ if($account->id>1){
+  // build companies table
+  $companies_table=new str_table(api_text("accounts_edit-companies-tr-unvalued"));
+  // build companies table headers
+  $companies_table->addHeader("&nbsp;",NULL,"16");
+  $companies_table->addHeader(api_text("accounts_edit-companies-th-name"),NULL,"100%");
+  $companies_table->addHeader(api_text("accounts_edit-companies-th-role"),"nowarp text-right");
+  $companies_table->addHeader("&nbsp;",NULL,"16");
+  // build companies table fields
+  foreach($account->companies as $company){
+   // make del and main
+   $del_td=api_link("submit.php?act=account_company_remove&idAccount=".$account->id."&idCompany=".$company->id,api_icon('icon-trash',api_text("accounts_edit-companies-td-remove")),NULL,NULL,FALSE,api_text("accounts_edit-companies-td-remove-confirm"));
+   if($company->main){
+    $main_td=api_icon('icon-star',api_text("accounts_edit-companies-td-main"));
+    if(count($account->companies)>1){$del_td="&nbsp;";}
+   }else{
+    $main_td=api_link("submit.php?act=account_company_mainize&idAccount=".$account->id."&idCompany=".$company->id,api_icon('icon-star-empty',api_text("accounts_edit-companies-td-mainize")));
+   }
+   // build company row
+   $companies_table->addRow();
+   $companies_table->addField($main_td,"text-center");
+   $companies_table->addField($company->name,"nowarp");
+   $companies_table->addField($company->role->name,"nowarp text-right");
+   $companies_table->addField($del_td,"text-center");
+  }
+  // get associated companies
+  $companies_array=array();
+  $companies=api_accounts_companies();
+  foreach($companies->results as $company){
+   // skip not administrable and assigned companies
+   if(!$company->administrator&&!api_accounts_account()->superuser){continue;}
+   //if(array_key_exists($company->id,$account->companies)){continue;}
+   $companies_array[]=$company;
+  }
+  if(count($companies_array)){
+   // build companies form
+   $companies_form=new str_form("submit.php?act=account_company_add&idAccount=".$account->id,"post","accounts_edit_companies");
+   $companies_box="<select name='idCompany' class='input-xlarge'>\n";
+   $companies_box.="<option value=''>".api_text("accounts_edit-companies-fo-add")."</option>\n";
+   foreach($companies_array as $company){
+    $companies_box.="<option value='".$company->id."'>".$company->name."</option>\n";
+   }
+   $companies_box.="</select>\n";
+   $companies_box.="<select name='idRole' class='input-medium'>\n";
+   foreach(api_accounts_roles()->results as $role){
+    $label=$role->level." &minus; ".$role->name;
+    if(strlen($role->description)){$label.=" (".$role->description.")";}
+    $companies_box.="<option value='".$role->id."'>".$label."</option>\n";
+   }
+   $companies_box.="</select>\n";
+   $companies_box.="<input type='submit' name='submit' class='btn' value='+'>\n";
+   $companies_form->addCustomField(NULL,$companies_box);
+  }
+ }
+ // companies groups
+ if($account->id>1){
+  $companies_groups_array=array();
+  // build options group
+  function options_group($array,$level=0){
+   foreach($array as $group){
+    $pre=NULL;
+    for($i=0;$i<$level;$i++){$pre.="&nbsp;&nbsp;&nbsp;";}
+    $return.="<option value='".$group->id."'>".$pre.$group->label."</option>\n";
+    $return.=options_group($group->groups,($level+1));
+   }
+   return $return;
+  }
+  // cycle companies
+  foreach($account->companies as $company){
+   // biuld company groups object
+   $company_groups=new stdClass();
+   $company_groups->idCompany=$company->id;
+   // build groups table
+   $groups_table=new str_table(api_text("accounts_edit-groups-tr-unvalued"));
+   // build companies table headers
+   $groups_table->addHeader("&nbsp;",NULL,"16");
+   $groups_table->addHeader(api_text("accounts_edit-groups-th-name",$company->name),NULL,"100%");
+   $groups_table->addHeader("&nbsp;",NULL,"16");
+   // build companies table fields
+   foreach($company->groups as $group){
+    // make del and main
+    $del_td=api_link("submit.php?act=account_group_remove&idAccount=".$account->id."&idCompany=".$company->id."&idGroup=".$group->id,api_icon('icon-trash',api_text("accounts_edit-groups-td-remove")),NULL,NULL,FALSE,api_text("accounts_edit-groups-td-remove-confirm"));
+    if($group->main){
+     $main_td=api_icon('icon-star',api_text("accounts_edit-groups-td-main"));
+     if(count($company->groups)>1){$del_td="&nbsp;";}
+    }else{
+     $main_td=api_link("submit.php?act=account_group_mainize&idAccount=".$account->id."&idCompany=".$company->id."&idGroup=".$group->id,api_icon('icon-star-empty',api_text("accounts_edit-groups-td-mainize")));
+    }
+    // build company row
+    $groups_table->addRow();
+    $groups_table->addField($main_td,"text-center");
+    $groups_table->addField($group->path.$group->label);
+    $groups_table->addField($del_td,"text-center");
+   }
+   // link table
+   $company_groups->table=$groups_table;
+   // get company groups
+   $groups=api_accounts_groups($company->id);
+   // build companies form
+   $groups_form=new str_form("submit.php?act=account_group_add&idAccount=".$account->id."&idCompany=".$company->id,"post","accounts_edit_company".$company->id."_groups");
+   $groups_box="<select name='idGroup' class='input-xlarge'>\n";
+   $groups_box.="<option value=''>".api_text("accounts_edit-groups-fo-add")."</option>\n";
+   $groups_box.=options_group($groups->results);
+   $groups_box.="</select>\n";
+   $groups_box.="<input type='submit' name='submit' class='btn' value='+'>\n";
+   $groups_form->addCustomField(NULL,$groups_box);
+   // link form
+   $company_groups->form=$groups_form;
+   // add element
+   $companies_groups_array[]=$company_groups;
+  }
+ }
+ // open split
  $GLOBALS['html']->split_open();
  $GLOBALS['html']->split_span(6);
- if($account->id){echo "<dl class='dl-horizontal'>\n<dt>".api_text("accounts_edit-ff-avatar")."</dt>\n</dd>\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='".api_accountAvatar($account->id)."?".rand(0,999)."' class='img-polaroid' width='125'>\n</dd>\n</dl>\n";}
- // build form
- $form=new str_form("submit.php?act=account_save&id=".$account->id,"post","accounts");
- $form->addField("text","name",api_text("accounts_edit-ff-name"),stripslashes($account->name),"input-large",api_text("accounts_edit-ff-name-placeholder"));
- // root checker
- if($account->id<>1){
-  $form->addField("text","account",api_text("accounts_edit-ff-account"),stripslashes($account->account),"input-xlarge",api_text("accounts_edit-ff-account-placeholder"));
-  $form->addField("select","typology",api_text("accounts_edit-ff-typology"),NULL,"input-medium");
-  $form->addFieldOption(0,api_text("accounts_edit-fo-disabled"),($account->typology==0)?TRUE:FALSE);
-  $form->addFieldOption(1,api_text("accounts_edit-fo-administrator"),($account->typology==1)?TRUE:FALSE);
-  $form->addFieldOption(2,api_text("accounts_edit-fo-user"),($account->typology==2)?TRUE:FALSE);
-  $form->addField("select","language",api_text("accounts_edit-ff-language"),NULL,"input-medium",NULL,$fo_array);
-  $form->addFieldOption("default","Default");
-  $dir="../languages/";
-  if(is_dir($dir)){
-   if($dh=opendir($dir)){
-    while(($file=readdir($dh))!==false){
-     if(substr($file,-4)==".xml" && $file<>"default.xml"){
-      $language=substr($file,0,-4);
-      $form->addFieldOption($language,$language,($language==$account->language)?TRUE:FALSE);
-     }
-    }
-    closedir($dh);
-   }
-  }
-  $form->addField("select","idCompany",api_text("accounts_edit-ff-company"),NULL,"input-large");
-  $form->addFieldOption('',api_text("accounts_edit-fo-CompanyNotAssigned"));
-  $companies=$GLOBALS['db']->query("SELECT * FROM accounts_companies ORDER BY company ASC,division ASC");
-  while($company=$GLOBALS['db']->fetchNextObject($companies)){
-   $form->addFieldOption($company->id,$company->company." - ".$company->division,($company->id==$account->idCompany)?TRUE:FALSE);
-  }
- }
- $form->addControl("submit",api_text("accounts_edit-fc-save"));
- $form->addControl("button",api_text("accounts_edit-fc-cancel"),NULL,"accounts_list.php");
- if($account->id>0 && api_checkPermission("accounts","accounts_delete")){
-  $form->addControl("button",api_text("accounts_edit-fc-delete"),"btn-danger","submit.php?act=account_delete&id=".$account->id,api_text("accounts_edit-fc-delete-confirm"));
- }
- if($account->id && (($_SESSION['account']->administrator && $account->id<>$_SESSION['account']->id) || $_SESSION['account']->id==1)){
-  $form->addControl("button",api_text("accounts_edit-fc-interpret"),"btn-warning","submit.php?act=account_interpret&idAccount=".$account->id,api_text("accounts_edit-fc-interpret-confirm"));
- }
- // show form
- $form->render();
- // split window
+ // renderize account dynamic list
+ if(is_object($account_dl)){$account_dl->render();}
+ // renderize account form
+ if(is_object($account_form)){$account_form->render();}
+ // split page
  $GLOBALS['html']->split_span(6);
- // if edit account show groups
- if($account->id>1){
-  // build table
-  $table=new str_table(api_text("accounts_edit-tr-unvalued"));
-  // table headers
-  $table->addHeader("&nbsp;",NULL,"16");
-  $table->addHeader(api_text("accounts_edit-th-group"),NULL,"100%");
-  $table->addHeader(api_text("accounts_edit-th-role"),"nowarp");
-  $table->addHeader("&nbsp;",NULL,"16");
-  // execute query
-  $query="SELECT accounts_groups.*,accounts_groups_join_accounts.idGrouprole,accounts_groups_join_accounts.main FROM accounts_groups_join_accounts JOIN accounts_groups ON accounts_groups_join_accounts.idGroup=accounts_groups.id WHERE accounts_groups_join_accounts.idAccount='".$account->id."' ORDER BY accounts_groups_join_accounts.main DESC, accounts_groups.idGroup ASC,accounts_groups.name ASC";
-  $groups=$GLOBALS['db']->query($query);
-  while($group=$GLOBALS['db']->fetchNextObject($groups)){
-   $name=$group->name;
-   if($group->idGroup>0){$name=api_groupName($group->idGroup)."&minus;".$name;}
-   if($group->description){$name.=" (".$group->description.")";}
-   // build group table row
-   $table->addRow();
-   // main group
-   if($group->main){
-    $main=api_icon('icon-star',api_text("accounts_edit-td-main"));
-   }else{
-    $main="<a href='submit.php?act=account_grouprole_main&idAccount=".$account->id."&idGroup=".$group->id."' title=\"".api_text("accounts_edit-td-mainMake")."\">".api_icon('icon-star-empty')."</a>";
-   }
-   // build table fields
-   $table->addField($main,"text-center");
-   $table->addField($name);
-   $table->addField(api_grouproleName($group->idGrouprole),"nowarp");
-   $table->addField("<a href=\"submit.php?act=account_grouprole_delete&idAccount=".$account->id."&idGroup=".$group->id."\" onClick=\"return confirm('".api_text("accounts_edit-td-groupDelete-confirm")."');\">".api_icon('icon-trash')."</a>","text-center");
+ // renderize companies table
+ if(is_object($companies_table)){$companies_table->render();}
+ // renderize companies form
+ if(is_object($companies_form)){$companies_form->render();}
+ // cycle associated companies
+ if(is_array($companies_groups_array)){
+  foreach($companies_groups_array as $company_groups){
+   // renderize groups table
+   if(is_object($company_groups->table)){$company_groups->table->render();}
+   // renderize groups form
+   if(is_object($company_groups->form)){$company_groups->form->render();}
   }
-  // show table
-  $table->render();
-  // add group form
- ?>
- <form class="form-horizontal" action="submit.php?act=account_grouprole_add&id=<?php echo $account->id;?>" method="post" name="accounts_groups">
-  <select class="span5" name="idGroup">
-   <option value="0">Aggiungi un gruppo</option>
-   <?php
-    $groups=$GLOBALS['db']->query("SELECT * FROM accounts_groups ORDER BY idGroup ASC,name ASC");
-    while($group=$GLOBALS['db']->fetchNextObject($groups)){
-     if(!api_checkAccountGroup($group->id,$account->id)){
-      $group_name=$group->name;
-      if($group->idGroup>0){$group_name=api_groupName($group->idGroup)."&minus;".$group_name;}
-      echo "<option value='".$group->id."'> ".$group_name;
-      if($group->description){echo " (".$group->description.")";}
-      echo "</option>\n";
-     }
-    }
-   ?>
-  </select>
-  <select class="span5" name="idGrouprole">
-   <?php
-    $grouproles=$GLOBALS['db']->query("SELECT * FROM accounts_grouproles ORDER BY id ASC");
-    while($grouprole=$GLOBALS['db']->fetchNextObject($grouproles)){
-     echo "<option value='".$grouprole->id."'";
-     if(api_accountGrouprole($group->id)==$grouprole->id){echo " selected";}
-     echo "> ".$grouprole->name;
-     if($grouprole->description){echo " (".$grouprole->description.")";}
-     echo "</option>\n";
-    }
-   ?>
-  </select>
-  <input type="submit" name="account_grouprole_add" class="btn" value="+">
- </form>
-<?php
  }
+ // close split
  $GLOBALS['html']->split_close();
+ // debug
+ if($_SESSION["account"]->debug){pre_var_dump($account,"print","account");}
 ?>
 <script type="text/javascript">
  $(document).ready(function(){
-  // validation
-  $('form[name=accounts]').validate({
+  // validation account
+  $('form[name=accounts_edit]').validate({
    rules:{
     name:{required:true,minlength:3},
-    account:{required:true,email:true}
+<?php
+ if(api_getOption("ldap")){
+  echo "    account:{email:true}\n";
+ }else{
+  echo "    account:{required:true,email:true}\n";
+ }
+?>
    },
    submitHandler:function(form){form.submit();}
   });
+  // validation companies
+  $('form[name=accounts_edit_companies]').validate({
+   rules:{
+    idCompany:{required:true}
+   },
+   submitHandler:function(form){form.submit();}
+  });
+<?php
+  // cycle companies
+  if(is_array($companies_groups_array)){
+   foreach($companies_groups_array as $company_groups){
+    echo "  // company ".$company_groups->idCompany." groups\n";
+    echo "  $('form[name=accounts_edit_company".$company_groups->idCompany."_groups]').validate({rules:{idGroup:{required:true}},submitHandler:function(form){form.submit();}});\n";
+   }
+  }
+?>
  });
 </script>
 <?php } ?>
--- accounts/accounts_list.php
+++ accounts/accounts_list.php
@@ -5,49 +5,51 @@
 $checkPermission="accounts_list";
 include("template.inc.php");
 function content(){
+ // definitions
+ $accounts_status_modals_array=array();
  // acquire variables
- $g_idCompany=$_GET['idCompany'];
- if(!isset($g_idCompany)){$g_idCompany=NULL;}
- $g_orderField=$_GET['of'];
- $g_orderMode=$_GET['om'];
+ $g_search=$_GET['q'];
+ // show filters
+ echo $GLOBALS['navigation']->filtersText();
  // build table
- $table=new str_table(api_text("list-tr-unvalued"),TRUE);
+ $table=new str_table(api_text("accounts_list-tr-unvalued"),TRUE);
  // build table header
  $table->addHeader("&nbsp;",NULL,"16");
- $table->addHeader("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;",NULL,"16");
- $table->addHeader(api_text("accounts_list-th-name"),"nowarp",NULL,"name");
- $table->addHeader(api_text("accounts_list-th-typology"),"nowarp",NULL,"typology");
- $table->addHeader(api_text("accounts_list-th-company"),"nowarp",NULL,"idCompany");
- $table->addHeader(api_text("accounts_list-th-account"),NULL,"100%","account");
- $table->addHeader(api_text("accounts_list-th-lastAccess"),"nowarp",NULL,"lastLogin");
- // build query
- $query_where="1";
- if($g_idCompany<>NULL){$query_where="idCompany='".$g_idCompany."'";}
- // query order
- $query_order=api_queryOrder("idCompany ASC,typology ASC,name ASC,account ASC");
- // execute query
- $accounts=$GLOBALS['db']->query("SELECT * FROM accounts_accounts WHERE ".$query_where.$query_order);
- while($account=$GLOBALS['db']->fetchNextObject($accounts)){
-  // switch typology
-  switch($account->typology){
-   case 0:$typology="Disabled";break;
-   case 1:$typology="Administrator";break;
-   case 2:$typology="User";break;
-  }
-  // make company name
-  if($account->idCompany>0){$company=api_companyName($account->idCompany);}else{$company="<i>".api_text("accounts_list-td-CompanyNotAssigned")."</i>";}
+ $table->addHeader(api_text("accounts_list-th-name"),"nowarp",NULL,"accounts_accounts.name");
+ $table->addHeader(api_text("accounts_list-th-account"),"nowarp",NULL,"accounts_accounts.account");
+ $table->addHeader(api_text("accounts_list-th-company"),"nowarp","100%");
+ if(api_getOption("ldap")){$table->addHeader(api_text("accounts_list-th-ldap"),"nowarp text-right",NULL,"accounts_accounts.ldapUsername");}
+ $table->addHeader(api_text("accounts_list-th-lastAccess"),"nowarp",NULL,"accounts_accounts.accDate");
+ $table->addHeader("&nbsp;",NULL,"16");
+ // get accounts
+ $accounts=api_accounts_accounts($g_search,FALSE); // pagination?
+ foreach($accounts->results as $account){
+  // build modal window
+  $accounts_status_modals_array[]=api_accounts_accountStatusModal($account);
+  // make class
+  if($account->id==$_GET['idAccount']){$tr_class="info";}else{$tr_class=NULL;}
+  // make companies
+  $companies=NULL;
+  foreach($account->companies as $company){$companies.=$company->name." (".$company->role->name.")"."<br>";}
   // build table row
-  $table->addRow();
+  $table->addRow($tr_class);
   // build table fields
-  $table->addField("<a href=\"accounts_edit.php?id=".$account->id."\">".api_icon('icon-edit')."</a>","nowarp");
-  $table->addField("<img src='".api_accountAvatar($account->id)."?".rand(0,999)."' style='width:16px;height:16px;'>");
+  $table->addField("<a href=\"accounts_edit.php?idAccount=".$account->id."\">".api_icon('icon-edit')."</a>","nowarp");
   $table->addField(stripslashes($account->name),"nowarp");
-  $table->addField($typology,"nowarp");
-  $table->addField($company,"nowarp");
-  $table->addField(stripslashes($account->account),"nowarp");
-  $table->addField(api_timestampFormat($account->lastLogin,api_text("datetime")),"nowarp");
+  $table->addField($account->account,"nowarp");
+  $table->addField(substr($companies,0,-4),"nowarp");
+  if(api_getOption("ldap")){$table->addField($account->ldap,"nowarp text-right");}
+  $table->addField(api_timestampFormat($account->accDate,api_text("datetime")),"nowarp");
+  $table->addField(end($accounts_status_modals_array)->link(api_icon($account->typology->icon,$account->typology->description)),"nowarp");
  }
  // show table
  $table->render();
+ // renderize status modal windows
+ foreach($accounts_status_modals_array as $status_modal){$status_modal->render();}
+ // debug
+ if($_SESSION["account"]->debug){
+  pre_var_dump($accounts->query,"print","query");
+  pre_var_dump($accounts->results,"print","accounts");
+ }
 }
 ?>
