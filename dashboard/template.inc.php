3be26adcae296b7a8f5a80b7f7c6f11ec61b504e
Manuel Zavatta <manuel.zavatta@gmail.com>
2017-01-23 11:35:15 +0100

    Template

--- dashboard/template.inc.php
+++ dashboard/template.inc.php
@@ -7,6 +7,13 @@ include("../core/api.inc.php");
 api_loadModule();
 // show header
 $html->header(api_text("module-title"),$module_name);
+// build navigation tab
+global $navigation;
+$navigation=new str_navigation();
+// dashboard edit
+$navigation->addTab(api_text("nav-dashboard-edit"),"dashboard_edit.php");
+// show navigation
+$navigation->render();
 // check permissions before displaying module
 if($checkPermission==NULL){content();}else{if(api_checkPermission($module_name,$checkPermission,TRUE)){content();}}
 // show footer
