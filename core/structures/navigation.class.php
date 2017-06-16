8298fd82c8d3ae3158e400e785cffd76359e07ca
Manuel Zavatta <manuel.zavatta@gmail.com>
2017-06-16 15:15:32 +0200

    Filters whitespaces fix

--- core/structures/navigation.class.php
+++ core/structures/navigation.class.php
@@ -184,7 +184,7 @@ class str_navigation{
      }else{*/
       if(is_array($_GET[$filter->name])){
        foreach($_GET[$filter->name] as $g_option){
-        $text_filter.=", ".$filter->options[$g_option];
+        $text_filter.=", ".trim(str_replace($filter->options[$g_option]));
        }
       }
       $value=substr($text_filter,2);
@@ -212,7 +212,7 @@ class str_navigation{
    if($value<>NULL){
     $text.=" <span class='label label-info'>";
     if($filter->label<>NULL && strtolower($filter->label)<>"&nbsp;"){$text.=$filter->label." = ";}
-    $text.=$value."</span>";
+    $text.=trim(str_replace("&nbsp;"," ",$value))."</span>";
    }
   }
   if($text<>NULL){
